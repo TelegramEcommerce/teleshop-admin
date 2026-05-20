f658cbf6a6e359fe8ad2141272eded71bc022bf2
SaiMaungLenn <maunglenn@gmail.com>
2026-05-20 20:11:44 +0630

    Updates

---
+++ assets/stats-C36tW1Xw.js
@@ -0,0 +1,1 @@
+import{s as a}from"./index-D73iPeG3.js";const o=t=>a.get("/stats",{params:t}).then(s=>s.data),r=t=>a.get("/stats/orders-by-day",{params:t}).then(s=>s.data),d=t=>a.get("/stats/top-products",{params:t}).then(s=>s.data);export{r as a,d as b,o as g};
